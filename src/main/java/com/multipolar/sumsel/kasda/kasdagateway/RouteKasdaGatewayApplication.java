45d2fca8d733ccfc2bfc405c944e0689df90e2b7
dimasm93 <software.dimas_m@icloud.com>
2020-09-07 10:07:03 +0700

    init project

---
+++ src/main/java/com/multipolar/sumsel/kasda/kasdagateway/RouteKasdaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.multipolar.sumsel.kasda.kasdagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteKasdaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteKasdaGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multipolar/sumsel/kasda/kasdagateway/RouteKasdaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multipolar.sumsel.kasda.kasdagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteKasdaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
